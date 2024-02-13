0196d85a0d08f3a18547a1fb8385e8c2a6f77422
Mr-Royyy <76892613+Mr-Royyy@users.noreply.github.com>
2024-02-12 20:55:20 -0500

    Update BrickBreaker.java

--- BrickBreaker.java
+++ BrickBreaker.java
@@ -85,14 +85,7 @@ public class BrickBreaker extends Application
         }
     }
 
-    /**
-     *
-     * @param primaryStage the primary stage for this application, onto which
-     * the application scene can be set. The primary stage will be embedded in
-     * the browser if the application was launched as an applet.
-     * Applications may create other stages, if needed, but they will not be
-     * primary stages and will not be embedded in the browser.
-     */
+   
     @Override
     public void start(Stage primaryStage)
     {
